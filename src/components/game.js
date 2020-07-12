8ee7cf6959a16f9376321e97d5ce57494af37cf3
Hari <hariprasadk125@gmail.com>
2020-07-12 18:37:25 +0530

    some corrections and ready to first deploy

--- src/components/game.js
+++ src/components/game.js
@@ -9,7 +9,7 @@ class Game extends React.Component {
         left : 5,
         guess : '',
         display : '?',
-        message:'Lets guess',
+        message:"Let's start guessing.....",
         style:{color:'white',border:'solid 2px white'}
     };
   }
@@ -34,32 +34,38 @@ class Game extends React.Component {
         }
         else if((number-2)<=input && input<=(number+2)){
             this.setState({
-                style:{color:'rgb(83, 225, 10)',border:'solid 2px rgb(83, 225, 10)'}
+                style:{color:'rgb(83, 225, 10)',border:'solid 2px rgb(83, 225, 10)'},
+                message:"You are too close...!"
             })
         }
         else if((number-5)<=input && input<=(number+5)){
             this.setState({
-                style:{color:'rgb(241, 255, 10)',border:'solid 2px rgb(241, 255, 10)'}
+                style:{color:'rgb(241, 255, 10)',border:'solid 2px rgb(241, 255, 10)'},
+                message:"You are close to it...!"
             })
         }
         else if((number-10)<=input && input<=(number+10)){
             this.setState({
-                style:{color:'yellow',border:'solid 2px yellow'}
+                style:{color:'yellow',border:'solid 2px yellow'},
+                message:"You are coming closer...!"
             })
         }
         else if((number-20)<=input && input<=(number+20)){
             this.setState({
-                style:{color:'orange',border:'solid 2px orange'}
+                style:{color:'orange',border:'solid 2px orange'},
+                message:"You are somewhere in middle way...!"
             })
         }
         else if((number-30)<=input && input<=(number+30)){
             this.setState({
-                style:{color:'rgb(253, 94, 2)',border:'solid 2px rgb(253, 94, 2)'}
+                style:{color:'rgb(253, 94, 2)',border:'solid 2px rgb(253, 94, 2)'},
+                message:"You are far from it...!"
             }) 
         }
         else{
             this.setState({
-                style:{color:'red',border:'solid 2px red'}
+                style:{color:'red',border:'solid 2px red'},
+                message:"You are too far...!"
             })   
         }
       }
@@ -82,17 +88,17 @@ class Game extends React.Component {
             <React.Fragment>
                 <div id="game" className="col-md-4 offset-md-4 text-light text-center">
                 <div className="game-title">
-                <h1>GUESS IT  {this.props.number}</h1>
-                <hr/>
+                <h1>GUESS IT</h1>
                 </div>   
-                <div>
+                <div id="chances">
                     <p>You have {this.state.left} chances left</p>
                 </div>
-                {/* <div id="message">
-                    <p>{this.state.message}</p>
-                </div> */}
                 <div id="display" >
                     <p style={this.state.style}>{this.state.display}</p>
+                </div>
+
+                <div id="message" style={this.state.style}>
+                    <p>{this.state.message}</p>
                 </div>
 
                 <div id="gameInput">
