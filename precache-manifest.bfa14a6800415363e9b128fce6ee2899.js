6b401465be35380e8b86a8120f4c7776ae4d5e2d
Hari <hariprasadk125@gmail.com>
2020-07-12 18:53:13 +0530

    Updates

---
+++ precache-manifest.bfa14a6800415363e9b128fce6ee2899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717e964c2daec3a26e8ff051c28e5a2b",
+    "url": "/Guess-It/index.html"
+  },
+  {
+    "revision": "84d61513742ab7ec89ec",
+    "url": "/Guess-It/static/css/main.0138c188.chunk.css"
+  },
+  {
+    "revision": "4380a9c3200afefdbc1b",
+    "url": "/Guess-It/static/js/2.fbec3323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Guess-It/static/js/2.fbec3323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d61513742ab7ec89ec",
+    "url": "/Guess-It/static/js/main.4b4525ed.chunk.js"
+  },
+  {
+    "revision": "99dd7dd8a4080fc58bd8",
+    "url": "/Guess-It/static/js/runtime-main.49419f77.js"
+  },
+  {
+    "revision": "4685f2493fb736f7e52f89600f155a27",
+    "url": "/Guess-It/static/media/sad.4685f249.jpg"
+  },
+  {
+    "revision": "c081c40c8d278d8749976995af3e1349",
+    "url": "/Guess-It/static/media/smile.c081c40c.jpg"
+  }
+]);
